e625ebd925d208f430880b38c78a2090ece56912
RompkoH <helio.rompkovski@landisgyr.com>
2024-07-23 22:36:12 -0300

    MainActivity

---
+++ app/src/main/java/com/example/orgs/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.orgs
+
+import android.app.Activity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val view = View(this)
+        setContentView(R.layout.activity_main)
+
+
+
+        
+        
+    }
+
+
+}
